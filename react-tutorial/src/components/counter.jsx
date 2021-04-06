e03c100358202418bd2b14ec881c8a466e62f5ac
sophiehu36 <sophiehu36@163.com>
2021-04-06 18:02:03 +0800

    Update counter.jsx

--- react-tutorial/src/components/counter.jsx
+++ react-tutorial/src/components/counter.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+    //controlled component doesn't has its own state, it is controlled by its parent component
     //state includes all the data the component needs
     //其他组件无法访问到state里的值
-    state = {
-        value: this.props.value,
-        //imageUrl: "https://picsum.photos/200",
-        tags: ["tag1", "tag2", "tag3"],
-    };
+    // state = {
+    //     value: this.props.counter.value,
+    //     //imageUrl: "https://picsum.photos/200",
+    //     tags: ["tag1", "tag2", "tag3"],
+    // };
 
     // 绑定this的原始办法，在constructor中用bind方法
     // constructor() {
@@ -19,17 +20,12 @@ class Counter extends Component {
     //用箭头函数解决this绑定问题
     //jsx的格式中无法直接调用函数或传参数，可以用箭头函数返回值的形式
     //onClick={() => this.handleIncrement(product)}
-    handleIncrement = (product) => {
-        //console.log(product);
-        // setState方法告诉react有东西改变了
-        this.setState({ value: this.state.value + 1 });
-    };
 
-    handleDecrement = (product) => {
-        //console.log(product);
-        // setState方法告诉react有state要改变了
-        this.setState({ value: this.state.value - 1 });
-    };
+    //handleDecrement = (product) => {
+    //console.log(product);
+    // setState方法告诉react有state要改变了
+    //    this.setState({ value: this.state.value - 1 });
+    //};
     // styles = {
     //     fontSize: "50px",
     //     fontWeight: "bold",
@@ -57,16 +53,17 @@ class Counter extends Component {
                     {this.formatCount()}
                 </span>
                 <button
-                    onClick={() => this.handleIncrement()}
+                    onClick={() => this.props.onIncrement(this.props.counter)}
                     className="btn btn-secondary btn-sm m-2"
                 >
                     Increment
                 </button>
                 <button
-                    onClick={() => this.handleDecrement()}
-                    className="btn btn-secondary btn-sm"
+                    //子组件发起一个事件，由父组件处理，事件通过this.props传递
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn-sm m-2"
                 >
-                    Decrement
+                    Delete
                 </button>
             </div>
         );
@@ -74,12 +71,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? "Zero" : value;
     }
 }
